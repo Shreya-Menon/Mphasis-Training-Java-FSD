ff0594a3c430d0bb1ce6cf45b226f2bc4fabb5f9
Shreya-Menon <shreyajshreyaj@gmail.com>
2026-03-28 12:03:20 +0530

    Add files via upload

---
+++ SpringCoreExp2/src/main/java/com/test/TestDemo1.java
@@ -0,0 +1,35 @@
+package com.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestDemo1 {
+
+	public static void main(String[] args) {
+		
+		/*ApplicationContext ctx=new ClassPathXmlApplicationContext("beans.xml");
+		Employee obj=ctx.getBean("emp",Employee.class);
+		System.out.println(obj);*/
+		
+		ApplicationContext ctx=new AnnotationConfigApplicationContext(AnnConfig.class);
+		Address adr1=ctx.getBean("getAdrObj",Address.class);
+		adr1.setCity("a");
+		adr1.setState("Indian state");
+		Address adr2=ctx.getBean("getAdrObj",Address.class);
+		adr2.setCity("m");
+		adr2.setState("asdf");
+		List<Address>list=Arrays.asList(adr1,adr2);
+		Employee emp=ctx.getBean("getEmployeeObj",Employee.class);
+		emp.setId(11);
+		emp.setName("a");
+		emp.setCompany("hfe");
+		emp.setAdr(list);
+		System.out.println(emp);
+
+	}
+
+}
