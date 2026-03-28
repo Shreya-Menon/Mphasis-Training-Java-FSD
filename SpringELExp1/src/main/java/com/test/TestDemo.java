4d96b97ca9fc147a2f2b71501e94dc0be12f7ad3
Shreya-Menon <shreyajshreyaj@gmail.com>
2026-03-28 12:04:27 +0530

    Add files via upload

---
+++ SpringELExp1/src/main/java/com/test/TestDemo.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("beans.xml");
+		Student obj=ctx.getBean("std",Student.class);
+		System.out.println(obj);
+		ExpressionParser exp=new SpelExpressionParser();
+		Expression ex=exp.parseExpression("'simple spring EL(SpEL) example'");
+		String data=ex.getValue(String.class);
+		System.out.println(data+"Literals");
+		Expression ex1=exp.parseExpression("'Simple spring EL(SpEL) example'".toUpperCase());
+		String dt=ex1.getValue(String.class);
+		System.out.println(dt);
+		Expression ex2=exp.parseExpression("500*2");
+		Integer v=ex2.getValue(Integer.class);
+		System.out.println(v);
+		
+	}
+
+}
