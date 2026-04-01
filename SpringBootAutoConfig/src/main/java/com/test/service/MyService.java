aa1147bf6de4e1960288257632c0f1b548a7693d
Shreya-Menon <shreyajshreyaj@gmail.com>
2026-04-01 17:52:39 +0530

    Add files via upload

---
+++ SpringBootAutoConfig/src/main/java/com/test/service/MyService.java
@@ -0,0 +1,11 @@
+package com.test.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+	public String message() {
+		return "Custom AutoConfig Response";
+	}
+	
+}
